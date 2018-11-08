a31b80bd94a3d0a9fbf4ea2e7f048fcdba6a524d
Tok <tok@common-services.com>
2018-11-08 10:26:29 +0700

    update

--- budden.js
+++ budden.js
@@ -9,3 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 });
+
+/* modal */
+var a = $('[data-toggle="modal"]');
+for (var i = 0; i < a.length; i++) {
+  $(a[i]).on('click', function() {
+    var target = $(this).attr('data-target');
+    $(target).css("display", "block");
+  });
+}
+
+$("span.close").on('click', function() {
+  var target = $(this).closest('.modal')[0];
+  $(target).css("display", "none");
+});
+
+/*
+window.onclick = function(event) {
+	console.log(event.target.id);
+}
+*/
